76fc3f33fa4a9d33a87d5b8a4c6332881649eb1d
“Alexis <alexisplettener@gmail.com>
2022-06-23 20:20:00 +0100

    testing the add button

--- src/index.test.ts
+++ src/index.test.ts
@@ -18,5 +18,21 @@ describe('index.html', () => {
 
     test('ul should have 5 li children', () => {
         expect(container.querySelector('ul').children.length).toBe(5)
-    })
+    });
+    test('button should add a li and return 6', () => {
+        const button = getByText(container, 'Añadir')
+        fireEvent.click(button);
+        expect(container.querySelector('ul').children.length).toBe(6);
+    });
+    test('button should add a li 2 times and return 7', () => {
+        const button = getByText(container, 'Añadir')
+        fireEvent.click(button);
+        fireEvent.click(button);
+        expect(container.querySelector('ul').children.length).toBe(7);
+    });
+    test('the new li should contain name Cordelia Ingram', () => {
+        const button = getByText(container, 'Añadir')
+        fireEvent.click(button);
+        expect(container.querySelector('ul :nth-child(6)').textContent).toBe('Cordelia Ingram');
+    });
 })
